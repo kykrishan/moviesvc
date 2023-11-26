ce8623ab2c5f53ff2f4eeaf337cb77a7ad1ce871
krishan yadav <kykrishanyadav@gmail.com>
2023-11-26 18:33:07 +0530

    added first endpoint

---
+++ src/main/java/com/alphas/movies/controller/MoviesController.java
@@ -0,0 +1,22 @@
+package com.alphas.movies.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MoviesController {
+
+    @GetMapping(value = "/test")
+    public String test(){
+        return "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<body>\n" +
+                "\n" +
+                "<h1>My First Heading</h1>\n" +
+                "<p>My first paragraph.</p>\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>";
+    }
+}
